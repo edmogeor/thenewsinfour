e6dbf7d64746756df0a1f7d599806dcd8293bca4
edmogeor <george.edmonds@ntlworld.com>
2018-10-08 13:26:49 +0100

    Add files via upload

---
+++ script/backend.js
@@ -0,0 +1,117 @@
+// © COPYRIGHT GEORGE EDMONDS 2018
+
+function loading() {
+  $("#page-loading").css("visibility", "visible");
+  $("#grid-parent").css("visibility", "hidden");
+  loadAPI();
+  artPicker();
+  getText();
+}
+
+function loadAPI () {
+  $.getJSON("https://newsapi.org/v2/top-headlines?country=gb&apiKey=fcfbb627476e4bf39968dbe306b6c333", artPicker);
+}
+
+var artSource, artHeadline, artDate, artChosen;
+
+function artPicker(result) {
+  if (result) {
+    var artNum = Math.floor(Math.random() * ((result.articles.length-1) - 0) + 0);
+    artChosen = result.articles[artNum].url;
+    if (artChosen == undefined) {
+      loading();
+    }
+    proxyurl = "https://cors-anywhere.herokuapp.com/";
+    artUrl = proxyurl + 'http://motyar.info/webscrapemaster/api/?url=' + artChosen + '&xpath=p'
+    $.getJSON(artUrl, getText);
+    artSource = result.articles[artNum].source.name
+    artHeadline = result.articles[artNum].title
+    artDate = result.articles[artNum].publishedAt
+    artDate = moment(artDate).format('hh:mm:ss MM/DD');
+  }
+}
+
+var maxwordlimit = 10;
+var minwordlimit = 4;
+
+var firstWord,secondWord,thirdWord,forthWord;
+
+var artWords = '';
+
+function getText(result) {
+  if (result) {
+    for (var i = result.length - 1; i >= 0; i--) {
+      delete result[i].class;
+      delete result[i].html;
+      result[i].text.replace(/<style.*<\/style>/g, '');
+      var tmp = document.createElement("DIV");
+      tmp.innerHTML = result[i].text;
+      result[i].text = tmp.textContent || tmp.innerText;
+      result[i].text = result[i].text.replace(/[^\w- ']/g,'');
+      result[i].text = result[i].text.replace(/\d\d(-)/g,'');
+      result[i].text = result[i].text.replace(/[0-9]/g, '');
+      result[i].text = result[i].text.replace(/\s\s+/g, ' ');
+      artWords += result[i].text + ' ';
+    }
+
+    artWords = artWords.split(' ')
+
+    if (artWords.length < 52) {
+      loading();
+    }
+
+    else if (artWords[52-1] != undefined) {
+      for (var j = artWords.length - 1; j >= 0; j--) {
+        var chars = artWords[j].split('');
+        if (chars.length > maxwordlimit || chars.length < minwordlimit ) {
+          artWords.splice(j, 1)
+        }
+      }
+
+      $("#word1").html(artWords[52-1] + '.');
+      $("#word2").html(artWords[21-1] + '.');
+      $("#word3").html(artWords[45-1] + '.');
+      $("#word4").html(artWords[39-1] + '.');
+
+      $("#source").html(artSource);
+      $("#headline").html('“' + artHeadline + '”');
+      $("#dateandtime").html(artDate);
+
+      scaler();
+
+      showPage();
+    }
+  }
+
+}
+
+function showPage() {
+  $("#page-loading").css("visibility", "hidden");
+  $("#grid-parent").css("visibility", "visible");
+}
+
+$(window).on('load', function () {
+  loading();
+});
+
+$(document).ready(function() {
+  $("#news_icon").click(function () {
+    window.open(artChosen, '_blank');
+  });
+  $("#reload_icon").click(function () {
+    loading();
+  });
+});
+
+//$(document).click(function() {
+//  loading();
+//});
+
+$(document).keypress(function(event){
+
+	var keycode = (event.keyCode ? event.keyCode : event.which);
+	if(keycode == '13'){
+		loading();
+	}
+
+});
---
+++ script/scaling.js
@@ -0,0 +1,79 @@
+// © COPYRIGHT GEORGE EDMONDS 2018
+
+var contentWidth = 1024;
+var contentHeight = 640;
+
+function scaler() {
+  var availableWidth = $( window ).width();
+  var availableHeight = $( window ).height();
+
+  var scale = Math.min(
+    availableWidth / contentWidth,
+    availableHeight / contentHeight
+  );
+
+  var gapscale = availableWidth / contentWidth,
+
+  fontscale = Math.max(Math.min(scale, 1.5), 0.5);
+  linescale = Math.max(Math.min(scale, 1.5), 1.3);
+  scale = Math.max(Math.min(scale, 1.6), 0.35);
+
+   $("h3").css("fontSize", 20*fontscale + "px");
+   $("h2").css("fontSize", 20*fontscale + "px");
+   $("h1").css("fontSize", 48*fontscale + "px");
+   $("h4").css("fontSize", 25*fontscale + "px");
+   $("h5").css("fontSize", 15*fontscale + "px");
+   $("h6").css("fontSize", 15*fontscale + "px");
+   $("h7").css("fontSize", 15*fontscale + "px");
+
+   $("#number").css("margin-top", 3.5*fontscale + "px");
+   $("#number").css("margin-left", 30*fontscale + "px");
+
+   $("h5").css("line-height", 0.8 * linescale);
+
+   $("#word-container1").css("margin-bottom", 30*fontscale + "px");
+   $("#word-container2").css("margin-bottom", 30*fontscale + "px");
+   $("#word-container3").css("margin-bottom", 30*fontscale + "px");
+
+   $("#source").css("margin-top", 20*fontscale + "px");
+   $("#headline").css("margin-top", 12.5*fontscale + "px");
+   $("#dateandtime").css("margin-top", 12.5*fontscale + "px");
+
+
+    $("#word-container1").css("border-bottom", 3*fontscale + "px" + " solid" + " black");
+    $("#word-container2").css("border-bottom", 6*fontscale + "px" + " solid" + " black");
+    $("#word-container3").css("border-bottom", 10*fontscale + "px" + " solid" + " black");
+    $("#word-container4").css("border-bottom", 13*fontscale + "px" + " solid" + " black");
+
+    $("#word-container1").css("padding-bottom", 15*fontscale + "px");
+    $("#word-container2").css("padding-bottom", 15*fontscale + "px");
+    $("#word-container3").css("padding-bottom", 15*fontscale + "px");
+    $("#word-container4").css("padding-bottom", 15*fontscale + "px");
+
+   $(".grid-container").css("margin", 76*scale + "px");
+   $(".grid-container").css("grid-template-columns", 410*scale + "px " + 315*scale + "px");
+   $(".grid-container").css("grid-gap", 127*scale + "px " + 150*gapscale + "px");
+
+   $("#news_icon").css("width", 60*fontscale + "px");
+   $("#reload_icon").css("width", 30*fontscale + "px");
+
+   var reloadmargin = 30*fontscale
+
+   $("#reload_icon").css("margin-top", reloadmargin + "px");
+
+   var infomargin = $("#words").height() - $("#info-container").height() - $("#reload_icon").height() - reloadmargin;
+   $("#info-container").css("margin-top", infomargin + "px");
+
+   $('#loading-animation').width(100*fontscale);
+   $('#loading-animatione').height(100*fontscale);
+
+}
+
+$(window).on('load', function () {
+  scaler();
+  console.log($("#reload_icon").height())
+});
+
+$( window ).resize(function() {
+  scaler();
+});
