d44a92a924d4a5851fc2848d7c7c4b927379de28
edmogeor <george.edmonds@ntlworld.com>
2018-10-10 20:21:30 +0100

    Add files via upload

--- script/backend.js
+++ script/backend.js
@@ -1,117 +1,106 @@
-// © COPYRIGHT GEORGE EDMONDS 2018
+//© COPYRIGHT GEORGE EDMONDS 2018
 
-function loading() {
-  $("#page-loading").css("visibility", "visible");
-  $("#grid-parent").css("visibility", "hidden");
-  loadAPI();
-  artPicker();
-  getText();
-}
+var sourcelist = "the-huffington-post,bbc-news,cnn,the-new-york-times,fox-news,nbc-news,the-guardian-uk,the-telegraph,independent,metro,mirror,"
 
-function loadAPI () {
-  $.getJSON("https://newsapi.org/v2/top-headlines?country=gb&apiKey=fcfbb627476e4bf39968dbe306b6c333", artPicker);
-}
+function getImage(){
+ $.getJSON('https://newsapi.org/v2/everything?sources=' + sourcelist + '&language=en&pageSize=100&apiKey=fcfbb627476e4bf39968dbe306b6c333', urlArray);
+};
 
-var artSource, artHeadline, artDate, artChosen;
+var imgURLs = []
+var pixelArray = []
 
-function artPicker(result) {
-  if (result) {
-    var artNum = Math.floor(Math.random() * ((result.articles.length-1) - 0) + 0);
-    artChosen = result.articles[artNum].url;
-    if (artChosen == undefined) {
-      loading();
-    }
-    proxyurl = "https://cors-anywhere.herokuapp.com/";
-    artUrl = proxyurl + 'http://motyar.info/webscrapemaster/api/?url=' + artChosen + '&xpath=p'
-    $.getJSON(artUrl, getText);
-    artSource = result.articles[artNum].source.name
-    artHeadline = result.articles[artNum].title
-    artDate = result.articles[artNum].publishedAt
-    artDate = moment(artDate).format('hh:mm:ss MM/DD');
-  }
-}
+var img = new Image();
+var canvas;
+var context;
 
-var maxwordlimit = 10;
-var minwordlimit = 4;
-
-var firstWord,secondWord,thirdWord,forthWord;
-
-var artWords = '';
-
-function getText(result) {
-  if (result) {
-    for (var i = result.length - 1; i >= 0; i--) {
-      delete result[i].class;
-      delete result[i].html;
-      result[i].text.replace(/<style.*<\/style>/g, '');
-      var tmp = document.createElement("DIV");
-      tmp.innerHTML = result[i].text;
-      result[i].text = tmp.textContent || tmp.innerText;
-      result[i].text = result[i].text.replace(/[^\w- ']/g,'');
-      result[i].text = result[i].text.replace(/\d\d(-)/g,'');
-      result[i].text = result[i].text.replace(/[0-9]/g, '');
-      result[i].text = result[i].text.replace(/\s\s+/g, ' ');
-      artWords += result[i].text + ' ';
-    }
-
-    artWords = artWords.split(' ')
-
-    if (artWords.length < 52) {
-      loading();
-    }
-
-    else if (artWords[52-1] != undefined) {
-      for (var j = artWords.length - 1; j >= 0; j--) {
-        var chars = artWords[j].split('');
-        if (chars.length > maxwordlimit || chars.length < minwordlimit ) {
-          artWords.splice(j, 1)
+function urlArray(data){
+  if (data) {
+    for (var i = 0; i < data.articles.length; i++) {
+      if (data.articles[i].urlToImage !== null) {
+        imgURLs[i] = {
+          articleURL: data.articles[i].url,
+          articleTime: moment(data.articles[i].publishedAt).format('DD/MM/YY HH:mm'),
+          articleSource: data.articles[i].source.name,
+          articleHeadline: data.articles[i].title,
+          imgURL: "https://cors-anywhere.herokuapp.com/" + data.articles[i].urlToImage
+          }
         }
       }
-
-      $("#word1").html(artWords[52-1] + '.');
-      $("#word2").html(artWords[21-1] + '.');
-      $("#word3").html(artWords[45-1] + '.');
-      $("#word4").html(artWords[39-1] + '.');
-
-      $("#source").html(artSource);
-      $("#headline").html('“' + artHeadline + '”');
-      $("#dateandtime").html(artDate);
-
-      scaler();
-
-      showPage();
+      makeCanvas();
     }
   }
 
-}
+  var canvas = [];
+  var context = [];
+  var images = [];
 
-function showPage() {
-  $("#page-loading").css("visibility", "hidden");
-  $("#grid-parent").css("visibility", "visible");
-}
+  var arrayLength = imgURLs.length
 
-$(window).on('load', function () {
-  loading();
-});
+  for (var j = 0, len = imgURLs.length; j < len; j++) {
+    var item = itemsToIterate[i];
+  }
 
-$(document).ready(function() {
-  $("#news_icon").click(function () {
-    window.open(artChosen, '_blank');
+var allInfo = [];
+
+function makeCanvas() {
+  for (var j = 0; j < imgURLs.length; j++) {
+    images[j] = new Image();
+    images[j].setAttribute('crossOrigin', '')
+    var newCanvas = document.createElement('canvas');
+    newCanvas.id = String(j);
+    newCanvas.className = "pixelcanvas"
+    var body = document.getElementsByTagName("body")[0];
+    body.appendChild(newCanvas);
+    canvas[j] = document.getElementById(String(j))
+    context[j] = canvas[j].getContext("2d");
+
+    if (imgURLs[j]) {
+      images[j].src = imgURLs[j].imgURL
+    }
+
+    if (images[j].src !== null) {
+      context[j].canvas.height = images[j].height
+      context[j].canvas.width = images[j].width
+    }
+
+  }
+
+  var rawpixelData = [];
+  var pixelData = [];
+  var pixellocation1 = 5221;
+  var pixellocation2 = 4539;
+
+  for (var n = 0; n < imgURLs.length; n++) {
+    context[n].drawImage(images[n], 0, 0);
+    if (context[n].canvas.width != 0 && context[n].canvas.height != 0) {
+      rawpixelData[n] = context[n].getImageData(0, 0, images[n].width, images[n].height).data;
+      pixelData[n] = {
+        avgred: Math.floor(((rawpixelData[n][pixellocation1*4]) + (rawpixelData[n][pixellocation2*4])) / 2),
+        avggreen: Math.floor(((rawpixelData[n][(pixellocation1*4)+1]) + (rawpixelData[n][(pixellocation1*4)+1])) /2),
+        avgblue: Math.floor(((rawpixelData[n][(pixellocation1*4)+2]) + (rawpixelData[n][(pixellocation1*4)+2])) /2),
+        avgalpha: Math.floor(((rawpixelData[n][(pixellocation1*4)+3]) + (rawpixelData[n][(pixellocation1*4)+3])) /2)
+      }
+      allInfo[n] = {
+        colour: "rgba(" + pixelData[n].avgred + ',' + pixelData[n].avggreen + ',' + pixelData[n].avgblue + ',' + pixelData[n].avgalpha + ')',
+        articleURL: imgURLs[n].articleURL,
+        articleTime: imgURLs[n].articleTime,
+        articleSource: imgURLs[n].articleSource,
+        articleHeadline: imgURLs[n].articleHeadline
+      }
+      allInfo[n].colour = tinycolor(allInfo[n].colour).saturate(amount = 52.214539).toString();
+    }
+  }
+
+  allInfo = allInfo.filter(function(x){
+    return (x !== (undefined || null || ''));
   });
-  $("#reload_icon").click(function () {
-    loading();
-  });
-});
 
-//$(document).click(function() {
-//  loading();
-//});
+  allInfo.sort(function(a, b){return a-b});
 
-$(document).keypress(function(event){
-
-	var keycode = (event.keyCode ? event.keyCode : event.which);
-	if(keycode == '13'){
-		loading();
-	}
-
-});
+  if (allInfo.length >= 49) {
+    setColour();
+  } else if (allInfo.length < 49) {
+    getImage();
+    urlArray();
+  }
+}
